c9910432a5f9bd01b189d5e774e7eb236b50d1f7
unknown <soffieanastyaa@gmail.com>
2023-05-27 00:23:50 +0700

    prisma generate

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/library/prismadb"
+import prisma from "../../../library/prismadb"
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
     if(req.method !== 'GET'){
         return res.status(405).end();
