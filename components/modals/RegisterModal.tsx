0593d1794eaaa9908d19ed14168c0e8f1a16687a
unknown <soffieanastyaa@gmail.com>
2023-05-26 22:26:48 +0700

    fixed v1

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -47,7 +47,7 @@ const RegisterModal = () => {
         }finally{
             setIsLoading(false)
         }
-    },[LoginModal, email, password, username, name])
+    },[email, password, username, name, registerModal])
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
